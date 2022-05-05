2e50d1ef063a36469553bc4f133913c721605294
Pavel Mezencev <mezencev24@mail.ru>
2022-05-05 16:30:31 +0300

    add some validation and local storage save data to calories cal

--- js/script.js
+++ js/script.js
@@ -442,27 +442,51 @@ window.addEventListener('DOMContentLoaded', () => {
   ///////// CALCULATOR ///////////////////////
 
   const calcEl = document.querySelector('.calculating__result span');
-  let sex = 'female',
+  let sex,
     weight,
     height,
     age,
+    ratio = localStorage.getItem('ratio') || 1.375;
+
+  if (!localStorage.getItem('sex')) {
+    sex = 'female';
+    localStorage.setItem('sex', 'female');
+  } else {
+    sex = localStorage.getItem('sex');
+  }
+
+  if (!localStorage.getItem('ratio')) {
     ratio = 1.375;
+    localStorage.setItem('ratio', 1.375);
+  } else {
+    ratio = localStorage.getItem('ratio');
+  }
 
   calcCalories();
+  initLocalData('#gender > div', 'calculating__choose-item_active');
+  initLocalData(
+    '.calculating__choose_big > div',
+    'calculating__choose-item_active'
+  );
 
-  getDynamicData('.calculating__choose_medium');
-  getStaticData('#gender', 'calculating__choose-item_active');
-  getStaticData('.calculating__choose_big', 'calculating__choose-item_active');
+  getDynamicData('.calculating__choose_medium > input');
+  getStaticData('#gender > div', 'calculating__choose-item_active');
+  getStaticData(
+    '.calculating__choose_big > div',
+    'calculating__choose-item_active'
+  );
 
   function getStaticData(selector, activClass) {
-    const elements = document.querySelectorAll(`${selector} > *`);
+    const elements = document.querySelectorAll(selector);
 
     elements.forEach((el) =>
       el.addEventListener('click', (e) => {
-        if (document.querySelector(selector).getAttribute('id') === 'gender') {
-          sex = e.target.getAttribute('id');
-        } else {
+        if (el.getAttribute('data-ratio')) {
           ratio = +e.target.dataset.ratio;
+          localStorage.setItem('ratio', ratio);
+        } else {
+          sex = e.target.getAttribute('id');
+          localStorage.setItem('sex', sex);
         }
 
         elements.forEach((el) => el.classList.remove(activClass));
@@ -473,13 +497,16 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   function getDynamicData(selector) {
-    const elements = document.querySelectorAll(`${selector} > input`);
+    const elements = document.querySelectorAll(selector);
 
     elements.forEach((el) =>
       el.addEventListener('input', (e) => {
         const id = e.target.getAttribute('id');
-        if (isNaN(+e.target.value)) {
+        if (e.target.value.match(/\D/g)) {
+          el.style.border = '1px solid red';
           return;
+        } else {
+          el.style.border = 'none';
         }
 
         switch (id) {
@@ -498,7 +525,7 @@ window.addEventListener('DOMContentLoaded', () => {
       })
     );
   }
-  console.log(calcEl);
+
   function calcCalories() {
     if (!sex || !weight || !height || !age || !ratio) {
       calcEl.textContent = '_____';
@@ -514,5 +541,20 @@ window.addEventListener('DOMContentLoaded', () => {
         (88.36 + 13.4 * weight + 4.8 * height - 5.7 * age) * ratio
       );
     }
+  }
+
+  function initLocalData(selector, activeClass) {
+    const elements = document.querySelectorAll(selector);
+
+    elements.forEach((el) => {
+      el.classList.remove(activeClass);
+
+      if (el.getAttribute('data-ratio') === localStorage.getItem('ratio')) {
+        el.classList.add(activeClass);
+      }
+      if (el.getAttribute('id') === localStorage.getItem('sex')) {
+        el.classList.add(activeClass);
+      }
+    });
   }
 });
